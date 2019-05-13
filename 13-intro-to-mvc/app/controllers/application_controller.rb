005ed30b4648562a633d928c637e458af3069582
kingcons <brit@kingcons.io>
2019-05-13 11:29:46 -0400

    Add finished code from lecture.

--- 13-intro-to-mvc/app/controllers/application_controller.rb
+++ 13-intro-to-mvc/app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < Sinatra::Base
+  set :views, "app/views"
 
+  # self.get '/' do
+  #   "Hello, llamas in pajamas"
+  # end
+
+  get '/' do
+    @books = Book.all
+    erb :index
+  end
+
+  get '/books/:id' do
+    @book = Book.find(params[:id])
+    erb :show
+  end
+
+  get '/cookies' do
+    "hi rose"
+  end
 end
